e169029870919e7c4a75f4ffd829b924a40abc64
Agustin <agustinsurila@gmail.com>
2022-02-22 20:52:07 -0300

    Initial Commit

---
+++ src/main.jsx
@@ -0,0 +1,24 @@
+import "./index.css";
+import { HashRouter as Router } from "react-router-dom";
+import { ChakraProvider } from "@chakra-ui/react";
+import React from "react";
+import ReactDOM from "react-dom";
+
+import App from "./App";
+import { ModalProvider } from "./Context/ModalContext";
+import { InputProvider } from "./Context/InputContext";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <ChakraProvider>
+      <ModalProvider>
+        <InputProvider>
+          <Router>
+            <App />
+          </Router>
+        </InputProvider>
+      </ModalProvider>
+    </ChakraProvider>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
