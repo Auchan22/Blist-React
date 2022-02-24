4e201189f724cda15943a7715ed78cb3d98cff9b
Agustin <agustinsurila@gmail.com>
2022-02-24 20:31:21 -0300

    Add Page Product Route

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import { Routes, Route } from "react-router-dom";
 import PageHome from "./Pages/PageHome";
 import PageError from "./Pages/PageError";
 import Layout from "./Pages/Layout";
+import PageProduct from "./Pages/PageProduct";
 
 function App() {
   return (
     <Layout>
       <Routes>
-        <Route element={<PageHome />} path="/" />
+        <Route exact element={<PageHome />} path="/" />
+        <Route element={<PageProduct />} path="/products/:productId" />
         <Route element={<PageError />} path="*" />
       </Routes>
     </Layout>
