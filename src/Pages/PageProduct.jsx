a686a1fc86c3ede4bb6e03ab54262ad1e2de0274
Agustin <agustinsurila@gmail.com>
2022-02-24 20:29:18 -0300

    Pagina Encargada de mostrar los detalles del producto en especifico

---
+++ src/Pages/PageProduct.jsx
@@ -0,0 +1,14 @@
+import { Box } from "@chakra-ui/react";
+import React from "react";
+
+import ProductDetail from "../components/ProductDetail";
+
+const PageProduct = () => {
+  return (
+    <Box minHeight={"50vh"}>
+      <ProductDetail />
+    </Box>
+  );
+};
+
+export default PageProduct;
