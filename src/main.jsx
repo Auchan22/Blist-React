c34512b765f351f2fc0234d86daf1e8bee479a62
Agustin <agustinsurila@gmail.com>
2022-02-24 20:31:57 -0300

    Changed HashRouter to BrowserRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 import "./index.css";
-import { HashRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import { ChakraProvider } from "@chakra-ui/react";
 import React from "react";
 import ReactDOM from "react-dom";
