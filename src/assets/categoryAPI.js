198fddb65880de9d0397eeb086dad3323554b3b9
Agustin <agustinsurila@gmail.com>
2022-02-24 00:22:25 -0300

    DB about categories

---
+++ src/assets/categoryAPI.js
@@ -0,0 +1,39 @@
+const CATEGORIES = [
+  {
+    catName: "Carne",
+    img: "../src/assets/FoodSVG/burger.svg",
+    id: "1",
+  },
+  {
+    catName: "Pollo",
+    img: "../src/assets/FoodSVG/chicken thigh.svg",
+    id: "2",
+  },
+  {
+    catName: "Fruta",
+    img: "../src/assets/FoodSVG/grape.svg",
+    id: "3",
+  },
+  {
+    catName: "Verdura",
+    img: "../src/assets/FoodSVG/carrot.svg",
+    id: "4",
+  },
+  {
+    catName: "Bebida",
+    img: "../src/assets/FoodSVG/cola.svg",
+    id: "5",
+  },
+  {
+    catName: "Lacteos",
+    img: "../src/assets/FoodSVG/burger.svg",
+    id: "6",
+  },
+  {
+    catName: "Postres",
+    img: "../src/assets/FoodSVG/ice cream.svg",
+    id: "7",
+  },
+];
+
+export default CATEGORIES;
