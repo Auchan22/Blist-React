699914ea55158c612a5e6ea8e8e43e1a77201315
Agustin <agustinsurila@gmail.com>
2022-02-24 00:28:16 -0300

    Added HandleSelect, and img to the item´s object

--- src/Context/InputContext.jsx
+++ src/Context/InputContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 import { useToast } from "@chakra-ui/react";
 
 const InputContext = createContext();
@@ -8,12 +8,34 @@ export default InputContext;
 const InputProvider = ({ children }) => {
   const toast = useToast();
 
-  const [items, setItems] = useState([]);
+  const [items, setItems] = useState(
+    JSON.parse(localStorage.getItem("Products")) || []
+  );
+  console.log(items);
+
+  const [img, setIMG] = useState("");
+
+  useEffect(() => {
+    localStorage.setItem("Products", JSON.stringify(items));
+  }, [items]);
 
   const [name, setName] = useState("");
+  const [category, setCategory] = useState("");
 
   const handleInputChange = (e) => {
     setName(e.target.value);
+  };
+
+  const handleSelect = (e) => {
+    setCategory(e.target.value);
+
+    let array = [...e.target.childNodes];
+    let selection = array.find(
+      (item) => item.getAttribute("value") === e.target.value
+    );
+    let selectionImg = selection.dataset.icon;
+    setIMG(selectionImg);
+    // console.log(selectionImg);
   };
 
   const handleSubmit = (e) => {
@@ -22,21 +44,30 @@ const InputProvider = ({ children }) => {
 
   const handleClick = () => {
     if (name === "") {
-      console.log("Hay un Error");
+      // console.log("Hay un Error");
+      toast({
+        title: "El campo Name debe estar completo",
+        status: "error",
+        duration: 1000,
+        isClosable: true,
+        position: "top",
+      });
     } else {
+      localStorage.setItem("Products", JSON.stringify(items));
       setItems(() => {
         return [
           ...items,
           {
             id: Date.now(),
             nombre: name,
-            imagen:
-              "https://via.placeholder.com/150/FFFF00/000000?Text=WebsiteBuilders.com",
+            imagen: img,
           },
         ];
       });
+      setName("");
       setTimeout(() => {
-        console.log("Tu elemento se agrego correctamente");
+        // console.log("Tu elemento se agrego correctamente");
+
         toast({
           title: "Item Creado Correctamente.",
           status: "success",
@@ -54,6 +85,7 @@ const InputProvider = ({ children }) => {
     handleSubmit,
     items,
     handleClick,
+    handleSelect,
   };
 
   return <InputContext.Provider value={data}>{children}</InputContext.Provider>;
