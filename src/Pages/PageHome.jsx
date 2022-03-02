cf195d039213d78260dd59e2b8abedb46b56fc16
Agustin <agustinsurila@gmail.com>
2022-03-01 22:45:27 -0300

    Renderizado condicional segun el max-width

--- src/Pages/PageHome.jsx
+++ src/Pages/PageHome.jsx
@@ -1,11 +1,22 @@
+import { useMediaQuery } from "@chakra-ui/react";
+
 import AddItem from "../components/AddItemBtn";
+import DeskInterface from "../components/DeskInterface";
 import ItemList from "../components/ItemList";
 
 const PageHome = () => {
+  const [isMobile] = useMediaQuery("(max-width: 450px)");
+
   return (
     <>
-      <ItemList />
-      <AddItem />
+      {isMobile ? (
+        <>
+          <ItemList />
+          <AddItem />
+        </>
+      ) : (
+        <DeskInterface />
+      )}
     </>
   );
 };
