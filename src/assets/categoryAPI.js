fe1c4e824a57820810a1c1c93694fb6e5a934117
Agustin <agustinsurila@gmail.com>
2022-03-02 14:51:33 -0300

    Updated the routes

--- src/assets/categoryAPI.js
+++ src/assets/categoryAPI.js
@@ -1,37 +1,37 @@
 const CATEGORIES = [
   {
     catName: "Carne",
-    img: "../src/assets/FoodSVG/burger.svg",
+    img: "../Images/FoodSVG/burger.svg",
     id: "1",
   },
   {
     catName: "Pollo",
-    img: "../src/assets/FoodSVG/chicken thigh.svg",
+    img: "../Images/FoodSVG/chicken thigh.svg",
     id: "2",
   },
   {
     catName: "Fruta",
-    img: "../src/assets/FoodSVG/grape.svg",
+    img: "../Images/FoodSVG/grape.svg",
     id: "3",
   },
   {
     catName: "Verdura",
-    img: "../src/assets/FoodSVG/carrot.svg",
+    img: "../Images/FoodSVG/carrot.svg",
     id: "4",
   },
   {
     catName: "Bebida",
-    img: "../src/assets/FoodSVG/cola.svg",
+    img: "../Images/FoodSVG/cola.svg",
     id: "5",
   },
   {
     catName: "Lacteos",
-    img: "../src/assets/FoodSVG/burger.svg",
+    img: "../Images/FoodSVG/burger.svg",
     id: "6",
   },
   {
     catName: "Postres",
-    img: "../src/assets/FoodSVG/ice cream.svg",
+    img: "../Images/FoodSVG/ice cream.svg",
     id: "7",
   },
 ];
