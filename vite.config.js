d321225f14ae2556db9e6be6069385a23efa6a71
Agustin <agustinsurila@gmail.com>
2022-03-02 15:46:48 -0300

    Add the root and public path

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,11 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  root: "./",
+  build: {
+    outDir: "dist",
+  },
+  publicDir: "public",
   plugins: [react()],
   server: {
     host: true,
